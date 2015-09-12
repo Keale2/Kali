5312cf4a8e2d18257db21091082ad52fb8dc526f
Kyle Wagner <Keale2@gmail.com>
2015-09-12 17:32:58 -0400

    more things!

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,16 @@
 // Main JS should go here!
 // Include scripts using Browserify by doing:
 // var $ = require('jquery');
+
+// this is so much fun i want to do this more often!
+
+ //alert("Hi!what up :).");
+
+
+var els = document.getElementsByClassName("changeMessage");
+
+[].forEach.call(els, function (el) {
+	el.addEventListener("click", function () {
+		el.innerText = el.getAttribute("data-message");
+	});
+});
